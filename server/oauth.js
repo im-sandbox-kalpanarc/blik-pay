08af151ab25c86dc9637a124eb86c3345034702c
Kalpana Reddy <Kalpana.c@infomagnus.com>
2023-08-17 13:37:57 +0530

    Blik pay latest code

---
+++ server/oauth.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+
+const { PAYPAL_API_BASE, CLIENT_ID, CLIENT_SECRET } = require("./config");
+
+async function getAccessToken() {
+  // print client id and secret
+  console.log(`🔑 Client ID: ${CLIENT_ID}`);
+  console.log(`🔑 Client Secret: ${CLIENT_SECRET}`);
+
+  const credentials = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
+
+  const { data } = await axios({
+    url: `${PAYPAL_API_BASE}/v1/oauth2/token`,
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Basic ${credentials}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: "grant_type=client_credentials",
+  });
+
+  return data;
+}
+
+module.exports = {
+  getAccessToken,
+};
